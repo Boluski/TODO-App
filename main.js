60bc7bee8c1b37a1cb6de378574cf31b5b593f8a
Boluwatife Ajibola <ajibolss11@gmail.com>
2022-04-30 11:06:55 -0400

    create makeTask function.

--- main.js
+++ main.js
@@ -1,2 +1,30 @@
-console.log('Hello World');
-console.log('JavaScript is working.');
+function getInput(){
+    let task = document.querySelector('.addTask').value;
+    return task;
+}
+
+function makeTask(taskName){
+    let container = document.querySelector('.container');
+    let taskDiv = document.createElement('div');
+    let name = document.createElement('p');
+    let completeTask = document.createElement('button');
+    let deleteTask = document.createElement('button');
+
+    name.textContent = taskName;
+    taskDiv.className = 'task';
+    deleteTask.className = 'delete';
+    deleteTask.textContent = 'D'
+    completeTask.className = 'mark';
+    completeTask.textContent = 'C'
+
+    taskDiv.appendChild(name);
+    taskDiv.appendChild(completeTask);
+    taskDiv.appendChild(deleteTask);
+    container.appendChild(taskDiv);
+}
+
+// run only when the return button is pressed
+function addNew(){
+    let newTask = getInput();
+    makeTask(newTask);
+}
